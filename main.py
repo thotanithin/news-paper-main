98903c710f32c9bda56d4dd6fe2145f3453f0719
thotanithin <thotanithin771@gmail.com>
2024-01-20 22:51:24 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import newspaper
+import nltk
+
+nltk.download('punkt')
+
+# st.markdown('<style> .css-1v0mbdj {margin:0 auto; width:50%; </style>', unsafe_allow_html=True)
+
+
+st.title('Article Summarizer')
+
+url = st.text_input('', placeholder='Paste the URL of the article amd press Enter')
+
+if url:
+    try:
+        article = newspaper.Article(url)
+
+        article.download()
+        # article.html
+        article.parse()
+
+        
+
+        title = article.title
+        st.subheader(title)
+        
+        date=article.publish_date
+        st.text(date)
+
+        authors = article.authors
+        st.text(','.join(authors))
+
+       
+
+        article.nlp()
+
+        keywords = article.keywords
+        st.subheader('Keywords:')
+        st.write(', '.join(keywords))
+        
+        tab1, tab2= st.tabs(["Full Text", "Summary"])
+        with tab1:
+            txt = article.text
+            txt = txt.replace('Advertisement', '')
+            st.write(txt)
+        
+        with tab2:
+            st.subheader('Summary')
+            summary = article.summary
+            summary = summary.replace('Advertisement', '')
+            st.write(summary)
+        
+        
+    except:
+        st.error('Sorry something went wrong')
